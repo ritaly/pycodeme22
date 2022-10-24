d5351ee01924842b056052efe359a4b63aa261e4
ritaly <malgorzata.lyczywek@gmail.com>
2022-10-24 20:05:07 +0200

    6 function

---
+++ 06_functions_cd/01.py
@@ -0,0 +1,39 @@
+def format_less_than_10(day): #tylko dodane formatowanie jeśli liczba 1, 2, 3, (< 10)
+    if day < 10:
+        return "0" + str(day)
+    else:
+        return day
+
+
+def show_month(name, days):
+    print(name)
+    print()
+    for day in days:
+        day += 1
+        if day % 7 == 0:
+            print(format_less_than_10(day))
+        else:
+            print(format_less_than_10(day), end="\t")
+    print()
+    print()
+
+
+data = [
+    ('January', range(31)),
+    ('February', range(28)),
+    ('March', range(31)),
+    ('April', range(30)),
+    ('May', range(31)),
+    ('June', range(30)),
+    ('July', range(31)),
+    ('August', range(31)),
+    ('September', range(30)),
+    ('October', range(31)),
+    ('November', range(30)),
+    ('December', range(31)),
+      ]
+
+data = dict(data)
+
+for name, days_range in data.items():
+    show_month(name, days_range)
