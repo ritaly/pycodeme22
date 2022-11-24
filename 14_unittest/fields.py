cfbdfa904f5b9752c6508d93d54500526c097b67
ritaly <malgorzata.lyczywek@gmail.com>
2022-11-24 18:29:10 +0100

    tests

--- 14_unittest/fields.py
+++ 14_unittest/fields.py
@@ -1,5 +1,5 @@
-def square_area(side):
-    return side * side
+def square_area(a):
+    return a * a
 
 
 def rectangle_area(a, b):
@@ -10,12 +10,16 @@ def triangle_area(a, b):
     return a * b * 0.5
 
 
+def trapezoid_area(a, b, h):
+    return (a+b) * h * 0.5
+
+
 def main():
     result_s = square_area(5)
     print(result_s == 25)
 
-    result_r = rectangle_area(5, 6)
-    print(result_r == 30)
+    result_r = rectangle_area(5, 'B')
+    print(result_r)
     print(result_r != 36)
 
 if __name__ == '__main__':
--- 14_unittest/fields_test.py
+++ 14_unittest/fields_test.py
@@ -1,14 +1,18 @@
 import unittest
-from fields import square_area, rectangle_area, triangle_area
+from fields import square_area, rectangle_area, triangle_area, trapezoid_area
 
 
 class FieldsTestCase(unittest.TestCase):
     def setUp(self):
         self.a = 50
         self.b = 60
+        self.h = 10
 
     def test_square_area_with_correct_values(self):
         self.assertEqual(square_area(self.a), 2500)
+
+    def test_square_area_with_incorrect_values(self):
+        self.assertEqual(square_area('$'), 25)
 
     def test_rectangle_area_with_correct_values(self):
         result = rectangle_area(self.a, self.b)
@@ -17,6 +21,10 @@ class FieldsTestCase(unittest.TestCase):
     def test_triangle_area_with_correct_values(self):
         result = triangle_area(self.a, self.b)
         self.assertEqual(result, 1500)
+
+    def test_trapezoid_area_with_correct_value(self):
+        result = trapezoid_area(self.a, self.b, self.h)
+        self.assertEqual(result, 550)
 
     def tearDown(self):
         del self.a
